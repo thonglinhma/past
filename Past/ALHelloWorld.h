4fcd6600246c2cdd2b7fc63af29d241cb3b249d4
Mike Tran <thonglinhma@hotmail.com>
2014-02-13 18:12:40 +0800

    hot fix

---
+++ Past/ALHelloWorld.h
@@ -0,0 +1,13 @@
+//
+//  ALHelloWorld.h
+//  Past
+//
+//  Created by Mike Tran on 2/13/14.
+//  Copyright (c) 2014 Abcdefghijk Lab. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ALHelloWorld : RVMViewModel
+
+@end
---
+++ Past/ALHelloWorld.m
@@ -0,0 +1,13 @@
+//
+//  ALHelloWorld.m
+//  Past
+//
+//  Created by Mike Tran on 2/13/14.
+//  Copyright (c) 2014 Abcdefghijk Lab. All rights reserved.
+//
+
+#import "ALHelloWorld.h"
+
+@implementation ALHelloWorld
+
+@end
